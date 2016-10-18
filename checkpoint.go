nestybox/sysbox-runc

commit 5dac33c0f7b0242debe8e83d7878121af858e64e

Container must not checkpoint in created state
Signed-off-by: rajasec <rajasec79@gmail.com>

checkpoint.go
@@ -38,6 +38,13 @@ checkpointed.`,
 		if err != nil {
 			return err
 		}
+		status, err := container.Status()
+		if err != nil {
+			return(err)
+		}
+		if status == libcontainer.Created {
+			fatalf("Container cannot be checkpointed in created state")
+		}
 		defer destroy(container)
 		options := criuOptions(context)
 		// these are the mandatory criu options for a container

commit 53b0181ae95a7366bd790e2ae809e80b853587b3

checkpoint: fix gofmt
Fixes: 5dac33c0f7b0 ("Container must not checkpoint in created state")
Fixes: #1076
Signed-off-by: Aleksa Sarai <asarai@suse.de>

checkpoint.go
@@ -40,7 +40,7 @@ checkpointed.`,
 		}
 		status, err := container.Status()
 		if err != nil {
-			return(err)
+			return err
 		}
 		if status == libcontainer.Created {
 			fatalf("Container cannot be checkpointed in created state")