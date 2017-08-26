nestybox/sysbox-runc

commit 1d77a1a0a43e754e8d5f0e2d2e2e2c8f22c6c177

Check error return values
Both tty.resize and notifySocket.setupSocket return an error which isn't
handled in the caller. Fix this and either log or propagate the errors.

Found using https://github.com/mvdan/unparam

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

signals.go
@@ -75,11 +75,15 @@ func (h *signalHandler) forward(process *libcontainer.Process, tty *tty, detach 
 	}
 
 	// perform the initial tty resize.
-	tty.resize()
+	if err := tty.resize(); err != nil {
+		logrus.Error(err)
+	}
 	for s := range h.signals {
 		switch s {
 		case unix.SIGWINCH:
-			tty.resize()
+			if err := tty.resize(); err != nil {
+				logrus.Error(err)
+			}
 		case unix.SIGCHLD:
 			exits, err := h.reap()
 			if err != nil {

utils_linux.go
@@ -371,7 +371,10 @@ func startContainer(context *cli.Context, spec *specs.Spec, action CtAct, criuOp
 	}
 
 	if notifySocket != nil {
-		notifySocket.setupSocket()
+		err := notifySocket.setupSocket()
+		if err != nil {
+			return -1, err
+		}
 	}
 
 	// Support on-demand socket activation by passing file descriptors into the container init process.

commit a086c6c5a11044f1198b0093c7b929edb4267dd6

signal: ignore tty.resize errors
Fixes a race that occurred very frequently in testing where the tty of
the container may be closed by the time that runc gets to sending
SIGWINCH. This failure mode is not fatal, but it would cause test
failures due to expected outputs not matching. On further review it
appears that the original addition of these checks in 1d77a1a0a43e
("Check error return values") was actually not necessary, so partially
revert that change.

The particular failure mode this resolves would manifest as error logs
of the form:

  time="2017-08-24T07:59:50Z" level=error msg="bad file descriptor"

Fixes: 1d77a1a0a43e ("Check error return values")
Signed-off-by: Aleksa Sarai <asarai@suse.de>

signals.go
@@ -74,16 +74,15 @@ func (h *signalHandler) forward(process *libcontainer.Process, tty *tty, detach 
 		}
 	}
 
-	// perform the initial tty resize.
-	if err := tty.resize(); err != nil {
-		logrus.Error(err)
-	}
+	// Perform the initial tty resize. Always ignore errors resizing because
+	// stdout might have disappeared (due to races with when SIGHUP is sent).
+	_ = tty.resize()
+	// Handle and forward signals.
 	for s := range h.signals {
 		switch s {
 		case unix.SIGWINCH:
-			if err := tty.resize(); err != nil {
-				logrus.Error(err)
-			}
+			// Ignore errors resizing, as above.
+			_ = tty.resize()
 		case unix.SIGCHLD:
 			exits, err := h.reap()
 			if err != nil {