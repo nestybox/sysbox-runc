nestybox/sysbox-runc

commit 60355ffb6b5ccfcf34c2cd7bb636cfa2ba32ddac

may kill other process when container has been stopped
Signed-off-by: Lifubang <lifubang@acmcoder.com>

libcontainer/container_linux.go
@@ -374,10 +374,18 @@ func (c *linuxContainer) Signal(s os.Signal, all bool) error {
 	if all {
 		return signalAllProcesses(c.cgroupManager, s)
 	}
-	if err := c.initProcess.signal(s); err != nil {
-		return newSystemErrorWithCause(err, "signaling init process")
+	status, err := c.currentStatus()
+	if err != nil {
+		return err
 	}
-	return nil
+	// to avoid a PID reuse attack
+	if status == Running || status == Created {
+		if err := c.initProcess.signal(s); err != nil {
+			return newSystemErrorWithCause(err, "signaling init process")
+		}
+		return nil
+	}
+	return newGenericError(fmt.Errorf("container not running"), ContainerNotRunning)
 }
 
 func (c *linuxContainer) createExecFifo() error {

commit 9ac44f33b43cd1d1623d668ec28072c713583fa4

kill: allow to signal paused containers
regression introduced by 60355ffb6b5ccfcf34c2cd7bb636cfa2ba32ddac

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

libcontainer/container_linux.go
@@ -382,7 +382,7 @@ func (c *linuxContainer) Signal(s os.Signal, all bool) error {
 		return err
 	}
 	// to avoid a PID reuse attack
-	if status == Running || status == Created {
+	if status == Running || status == Created || status == Paused {
 		if err := c.initProcess.signal(s); err != nil {
 			return newSystemErrorWithCause(err, "signaling init process")
 		}