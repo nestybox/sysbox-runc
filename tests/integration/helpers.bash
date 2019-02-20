aeb6b79263ee74442ca3562024a9649ab911c690
ctv10 <ctalledo@gmail.com>
2019-02-19 19:36:19 -0800

    Fixed tests that were broken during runc -> syscont-runc port; plus minor updates to to-do list.

--- tests/integration/helpers.bash
+++ tests/integration/helpers.bash
@@ -5,7 +5,7 @@ INTEGRATION_ROOT=$(dirname "$(readlink -f "$BASH_SOURCE")")
 
 . ${INTEGRATION_ROOT}/multi-arch.bash
 
-RUNC="${INTEGRATION_ROOT}/../../runc"
+RUNC="${INTEGRATION_ROOT}/../../syscont-runc"
 RECVTTY="${INTEGRATION_ROOT}/../../contrib/cmd/recvtty/recvtty"
 GOPATH="$(mktemp -d --tmpdir runc-integration-gopath.XXXXXX)"
 
