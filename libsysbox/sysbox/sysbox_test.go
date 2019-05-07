6584ce96880fe7172324745ad8a1b867611bf1c6
Cesar Talledo <ctalledo@nestybox.com>
2019-05-07 05:05:20 +0000

    Fixed bug in code that checks for supported kernel versions.

---
+++ libsysbox/sysbox/sysbox_test.go
@@ -0,0 +1,30 @@
+package sysbox
+
+import "testing"
+
+func TestIsKernelSupported(t *testing.T) {
+
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"4.10", true},
+		{"4.9", false},
+		{"4.11", true},
+		{"3.0", false},
+		{"5.1", true},
+		{"4.18.0-17", true},
+		{"4.10.5", true},
+		{"4.9.20", false},
+	}
+
+	for _, test := range tests {
+		got, err := IsKernelSupported(test.input)
+		if err != nil {
+			t.Errorf("IsKernelSupported(%q) returned error %v", test.input, err)
+		}
+		if got != test.want {
+			t.Errorf("IsKernelSupported(%q) = %v", test.input, got)
+		}
+	}
+}
