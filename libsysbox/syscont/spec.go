e91d1c4513f7f695dbd3e4c0c6897cd816492cda
Cesar Talledo <ctalledo@nestybox.com>
2019-11-08 22:05:21 +0000

    Revert "Added userns-remap mode override per sys container."
    This change largely reverts commit b31f9aab923fd3e9c352dabe2d5aca6580a8413c,
    as we've decided to not support system container identity userns-remap mode
    due to security concerns.

--- libsysbox/syscont/spec.go
+++ libsysbox/syscont/spec.go
@@ -142,11 +142,6 @@ var sysboxSystemdEnvVars = []string{
 	"container=private-users",
 }
 
-// sysbox configuration env vars
-var sysboxCfgEnvVars = []string{
-	"SYSBOX_USERNS_REMAP",
-}
-
 // sysbox's generic mount requirements
 var sysboxMounts = []specs.Mount{
 	// we don't yet support /dev/kmsg; create a dummy one.
@@ -289,65 +284,19 @@ func cfgNamespaces(spec *specs.Spec) error {
 	return nil
 }
 
-// getEnvVar searches for a given environment variable in sys container's process spec and
-// returns its value. If none if found, returns ("", false).
-func getEnvVar(spec *specs.Spec, name string) (string, bool) {
-	for _, str := range spec.Process.Env {
-		tokens := strings.SplitN(str, "=", 2)
-		if len(tokens) != 2 {
-			return "", false
-		}
-		key := tokens[0]
-		val := tokens[1]
-		if key == name {
-			return val, true
-		}
-	}
-	return "", false
-}
-
-// mustUseIdentityMap returns true if the system container must use userns-remap identity mode.
-func mustUseIdentityMap(spec *specs.Spec) bool {
-	// Mounts over the system container "/var/lib/docker" require identity map mode because
-	// otherwise shiftfs would need to be mounted on the source of the mount, and then
-	// Docker inside the sys container will mount overlayfs on top, and overlayfs can't be
-	// mounted on shiftfs (yet); see Sysbox issue #93.
-	for _, m := range spec.Mounts {
-		if m.Destination == "/var/lib/docker" {
-			return true
-		}
-	}
-	return false
-}
-
 // allocIDMappings performs uid and gid allocation for the system container
 func allocIDMappings(sysMgr *sysbox.Mgr, spec *specs.Spec) error {
 	var uid, gid uint32
 	var err error
 
-	sysboxUsernsRemapEnv, _ := getEnvVar(spec, "SYSBOX_USERNS_REMAP")
-
-	allocMode := ""
-	if mustUseIdentityMap(spec) {
-		allocMode = "identity"
-	} else if sysboxUsernsRemapEnv != "" {
-		allocMode = sysboxUsernsRemapEnv
-	}
-
 	if sysMgr.Enabled() {
-		uid, gid, err = sysMgr.ReqSubid(IdRangeMin, allocMode)
+		uid, gid, err = sysMgr.ReqSubid(IdRangeMin)
 		if err != nil {
 			return fmt.Errorf("subid allocation failed: %v", err)
 		}
 	} else {
-		// sysbox-runc unit tests
-		if allocMode == "identity" {
-			uid = 0
-			gid = 0
-		} else {
-			uid = defaultUid
-			gid = defaultGid
-		}
+		uid = defaultUid
+		gid = defaultGid
 	}
 
 	uidMap := specs.LinuxIDMapping{
@@ -397,7 +346,8 @@ func validateIDMappings(spec *specs.Spec) error {
 }
 
 // cfgIDMappings checks if the uid/gid mappings are present and valid; if they are not
-// present, it allocates them.
+// present, it allocates them. Note that we don't disallow mappings that map to the host
+// root UID (i.e., identity-mappings); some runc tests use such mappings.
 func cfgIDMappings(sysMgr *sysbox.Mgr, spec *specs.Spec) error {
 	if len(spec.Linux.UIDMappings) == 0 && len(spec.Linux.GIDMappings) == 0 {
 		return allocIDMappings(sysMgr, spec)
@@ -781,19 +731,6 @@ func getSupConfig(mgr *sysbox.Mgr, spec *specs.Spec, shiftUids bool) error {
 	return nil
 }
 
-// removeSysboxCfgEnvVars removes sysbox config env vars from the sys container's process spec.
-func removeSysboxCfgEnvVars(p *specs.Process) {
-	for _, env := range sysboxCfgEnvVars {
-		p.Env = stringSliceRemoveMatch(p.Env, func(v string) bool {
-			tokens := strings.SplitN(v, "=", 2)
-			if len(tokens) != 2 {
-				return false
-			}
-			return tokens[0] == env
-		})
-	}
-}
-
 // Configure the container's process spec for system containers
 func ConvertProcessSpec(p *specs.Process) error {
 	cfgCapabilities(p)
@@ -802,7 +739,6 @@ func ConvertProcessSpec(p *specs.Process) error {
 		return fmt.Errorf("failed to configure AppArmor profile: %v", err)
 	}
 
-	removeSysboxCfgEnvVars(p)
 	return nil
 }
 
