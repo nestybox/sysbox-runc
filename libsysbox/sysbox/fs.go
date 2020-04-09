7eaa3ad8d028358990ee9ebbd8e5c4645e772c6b
Rodny Molina <rmolina@nestybox.com>
2020-04-08 17:25:15 -0800

    Add support for multi-fuse-server funtionality

--- libsysbox/sysbox/fs.go
+++ libsysbox/sysbox/fs.go
@@ -42,11 +42,29 @@ func (fs *Fs) Enabled() bool {
 	return fs.Active
 }
 
-// Registers container info with with sysbox-fs
+// Pre-registers container with sysbox-fs.
+func (fs *Fs) PreRegister() error {
+	if fs.Reg {
+		return fmt.Errorf("container %v already registered", fs.Id)
+	}
+
+	data := &sysboxFsGrpc.ContainerData{
+		Id: fs.Id,
+	}
+
+	if err := sysboxFsGrpc.SendContainerPreRegistration(data); err != nil {
+		return fmt.Errorf("failed to pre-register with sysbox-fs: %v", err)
+	}
+
+	return nil
+}
+
+// Registers container with sysbox-fs.
 func (fs *Fs) Register(info *FsRegInfo) error {
 	if fs.Reg {
 		return fmt.Errorf("container %v already registered", fs.Id)
 	}
+
 	data := &sysboxFsGrpc.ContainerData{
 		Id:            fs.Id,
 		InitPid:       int32(info.Pid),
@@ -58,10 +76,13 @@ func (fs *Fs) Register(info *FsRegInfo) error {
 		ProcRoPaths:   info.ProcRoPaths,
 		ProcMaskPaths: info.ProcMaskPaths,
 	}
+
 	if err := sysboxFsGrpc.SendContainerRegistration(data); err != nil {
 		return fmt.Errorf("failed to register with sysbox-fs: %v", err)
 	}
+
 	fs.Reg = true
+
 	return nil
 }
 
