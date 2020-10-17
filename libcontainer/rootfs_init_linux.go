affae63b81d447090fd55551eb8760117649baa7
Cesar Talledo <ctalledo@nestybox.com>
2020-10-17 16:02:56 -0700

    Use Linux mount command as a backup in case mount syscall fails.
    sysbox-runc fails to setup bind mounts into the container's
    rootfs when the source of the mount is a directory on shiftfs on tmpfs.
    
    Specifically, the mount syscall returns EPERM, though it's unclear why (likely
    an interaction between shiftfs and tmpfs that we don't fully understand yet).
    
    As a work-around this change causes sysbox-runc to use the Linux mount command
    (instead of the mount syscall) when the mount syscall fails. The Linux mount
    command is doing some operation prior to issuing the mount syscall itself that
    is causing the latter to work without problem.
    
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- libcontainer/rootfs_init_linux.go
+++ libcontainer/rootfs_init_linux.go
@@ -10,9 +10,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/Masterminds/semver"
 	"github.com/opencontainers/runc/libcontainer/configs"
 	"github.com/opencontainers/selinux/go-selinux/label"
-	"github.com/Masterminds/semver"
 	"golang.org/x/sys/unix"
 )
 
@@ -74,7 +74,7 @@ func iptablesRestoreHasWait() (bool, error) {
 func doBindMount(m *configs.Mount) error {
 
 	// sysbox-runc: For some reason, when the rootfs is on shiftfs, we
-	// need to do an Lstat() of the destination path prior to doing the
+	// need to do an Lstat() of the source path prior to doing the
 	// mount. Otherwise we get a "permission denied" error. It took me
 	// a while to figure this out. I found out by noticing that the
 	// mount cmd (not the syscall) would not hit the permission error,
@@ -83,13 +83,24 @@ func doBindMount(m *configs.Mount) error {
 	// the problem. FYI, in order to do the strace, I had to enable the
 	// ptrace syscall inside the container (via the libsysbox's syscalls.go).
 
-	_, err := os.Lstat(filepath.Dir(m.Source))
-	if err != nil {
-		return err
+	src := m.Source
+	if !m.BindSrcInfo.IsDir {
+		src = filepath.Dir(m.Source)
 	}
 
+	os.Lstat(src)
 	if err := unix.Mount(m.Source, m.Destination, "", unix.MS_BIND|unix.MS_REC, ""); err != nil {
-		return fmt.Errorf("bind-mount of %s to %s failed: %v", m.Source, m.Destination, err)
+
+		// We've noticed that the lstat and/or mount syscall fails with EPERM when
+		// bind-mounting a source dir that is on a shiftfs mount on top of a tmpfs
+		// mount. For some reason the Linux "mount" command does not fail in this case,
+		// so let's try it.
+
+		cmd := exec.Command("/bin/mount", "--rbind", m.Source, m.Destination)
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("bind-mount of %s to %s failed: %v", m.Source, m.Destination, err)
+		}
 	}
 
 	for _, pflag := range m.PropagationFlags {
@@ -235,7 +246,7 @@ func (l *linuxRootfsInit) Init() error {
 	switch l.reqs[0].Op {
 	case bind:
 
-		// The mount requests assume that the process cwd be the rootfs directory
+		// The mount requests assume that the process cwd is the rootfs directory
 		rootfs := l.reqs[0].Rootfs
 		if err := unix.Chdir(rootfs); err != nil {
 			return newSystemErrorWithCausef(err, "chdir to rootfs %s", rootfs)
